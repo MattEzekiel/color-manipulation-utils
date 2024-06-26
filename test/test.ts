4480e514cc7b2f5acfadf41dcb31744be9e51326
Matias Bubello <matt.bubello@gmail.com>
2024-06-26 09:13:52 -0300

    npm ignore and test

---
+++ test/test.ts
@@ -0,0 +1,4 @@
+import { lighten, darken } from "../index";
+
+console.log(lighten('#7a0f0f', 0.2));
+console.log(darken('#068806', 0.8));
---
+++ test/testDefault.ts
@@ -0,0 +1,5 @@
+import lighten from "../lighten/index";
+import darken from "../darken/index";
+
+console.log(lighten('#7a0f0f', 0.2));
+console.log(darken('#ffaf30', 0.2));
